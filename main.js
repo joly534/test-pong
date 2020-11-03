77f267c494ae68405f70171c99508a3527688f99
jonathan joly <joly534@gmail.com>
2020-11-03 15:36:35 +0100

    ajout plateau de jeu

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var game =true;
 canvas.width=600;
 canvas.height=400;
 
-var Objet = function(x,y,vx,vy){
+var Balle = function(x,y,vx,vy){
     this.x=x;
     this.y=y;
     this.vx=vx;
@@ -19,16 +19,40 @@ var Objet = function(x,y,vx,vy){
     this.move = function(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
         x += vx;
-        if ((x + this.rayon == canvas.width)||(x== 0)){           
+        if ((x + this.rayon == canvas.width)||(x+vx== 0)){           
             vx = -vx;
         }
     }
 };
-var balle=new Objet(100,200,2,2);
+
+var Plateau = function(x,y,width,height){
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.draw = function(){
+        ctx.rect(x,y,width,height);
+        ctx.fillStyle='white';
+        ctx.fill();
+    };
+    this.move = function(){
+        canvas.addEventListener('mousemove', controlSouris);
+        function controlSouris(e){
+            y = e.offsetY;
+        }
+    }
+}
+
+
+
+var balle=new Balle(100,200,2,2);
+var player = new Plateau(10,200,20,100);
 
 function update(){
     balle.move();
     balle.draw();
+    player.draw();
+    player.move();
     window.requestAnimationFrame(update);
 
 }
